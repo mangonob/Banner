e93c5cdd177ec5c39afcba6707e86a26271b5141
mangonob <534393806@qq.com>
2018-05-09 18:14:57 +0800

    M Banner

--- Banner/CCBanner.swift
+++ Banner/CCBanner.swift
@@ -10,17 +10,19 @@ import UIKit
 
 
 @objc protocol CCDrawable {
-    func drawableView(_ view: UIView, atRect rect: CGRect)
+    @objc optional func drawableView(_ view: UIView, atRect rect: CGRect)
 }
 
 @objc protocol CCBannerDelegate: CCDrawable {
-    @objc func banner(_ banner: CCBanner, selectedAtIndex index: Int)
-    @objc func banner(_ banner: CCBanner, insetOfImageAtIndex: Int)
+    @objc optional func banner(_ banner: CCBanner, selectedAtIndex index: Int)
+    @objc optional func banner(_ banner: CCBanner, insetOfImageAtIndex index: Int) -> UIEdgeInsets
+    @objc optional func banner(_ banner: CCBanner, drawableAtIndex index: Int) -> CCDrawable?
+    @objc optional func banner(_ banner: CCBanner, contentModeAtIndex index: Int) -> UIViewContentMode
 }
 
 protocol CCBannerDataSource: AnyObject {
     func numberOfImages(inBanner banner: CCBanner) -> Int
-    func banner(_ banner: CCBanner, imageAtIndex: Int) -> CCBannerImage?
+    func banner(_ banner: CCBanner, imageAtIndex index: Int) -> CCBannerImage?
 }
 
 enum CCBannerImage {
@@ -29,21 +31,187 @@ enum CCBannerImage {
 }
 
 class CCBanner: UIControl, CCDrawable {
-    var delegate: CCBannerDelegate?
-    var dataSource: CCBannerDataSource?
-    var imageInsets: UIEdgeInsets = .init(top: 10, left: 10, bottom: 10, right: 10)
+    var delegate: CCBannerDelegate? {
+        didSet {
+            setNeedsDisplay()
+            cover.drawable = delegate
+        }
+    }
+
+    var dataSource: CCBannerDataSource? {
+        didSet {
+            reloadBanner()
+        }
+    }
+    
+    var imageInsets: UIEdgeInsets = .zero
+    var images: [CCBannerImage?]?
+    
+    var isCircle: Bool = true {
+        didSet {
+        }
+    }
+
+    var isAutoRuning: Bool = true {
+        didSet {
+        }
+    }
+    
+    var runningDuration: TimeInterval = 1.5 {
+        didSet {
+        }
+    }
+
+    private lazy var prepareOnce: Void = {
+        currentIndex = 0
+    }()
+    
+    private var originalOffset: Int = 0
+    
+    var progress: Double = 0 {
+        didSet {
+            guard progress != oldValue else { return }
+            setNeedsDisplay()
+            
+            if progress == -1 || progress == 1 {
+                currentIndex += Int(progress)
+                withOutObserving {
+                    if isCircle {
+                        scrollView.contentOffset.x = bounds.width
+                        originalOffset = 1
+                    } else {
+                        if currentIndex == 0 {
+                            scrollView.contentOffset = .zero
+                            originalOffset = 0
+                        } else if currentIndex == numberOfImages - 1 {
+                            scrollView.contentOffset.x = bounds.width * 2
+                            originalOffset = 2
+                        } else {
+                            scrollView.contentOffset.x = bounds.width
+                            originalOffset = 1
+                        }
+                    }
+                }
+            } else if progress == 0 {
+                _ = prepareOnce
+            }
+        }
+    }
+
+    fileprivate lazy var cover: CCBannerCoverView = {
+        let cover = CCBannerCoverView()
+        cover.backgroundColor = .clear
+        addSubview(cover)
+        cover.isUserInteractionEnabled = false
+        cover.drawable = self
+        return cover
+    }()
+
+    private var numberOfImages: Int {
+        return dataSource?.numberOfImages(inBanner: self) ?? images?.count ?? 0
+    }
+    
+    private func imageAtIndex(_ index: Int) -> CCBannerImage? {
+        return dataSource?.banner(self, imageAtIndex: index) ?? images?[index]
+    }
+    
+    private func imageInsetsAtIndex(_ index: Int) -> UIEdgeInsets {
+        return delegate?.banner?(self, insetOfImageAtIndex: index) ?? imageInsets
+    }
+    
+    private func drawableAtIndex(_ index: Int) -> CCDrawable? {
+        return delegate?.banner?(self, drawableAtIndex: index)
+    }
+    
+    private func contentModeAtIndex(_ index: Int) -> UIViewContentMode {
+        return delegate?.banner?(self, contentModeAtIndex: index) ?? contentMode
+    }
 
     fileprivate lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView(frame: bounds)
-        addSubview(scrollView)
-        bannerViews.forEach { scrollView.addSubview($0) }
+        insertSubview(scrollView, at: 0)
+        scrollView.isPagingEnabled = true
         return scrollView
     }()
     
-    let bannerViews: [CCBannerView] = (0..<3).map { _ in CCBannerView() }
+    fileprivate lazy var bannerViews: [CCBannerView] = {
+        let bannerViews = (0..<3).map { (_) -> CCBannerView in
+            let banner = CCBannerView()
+            scrollView.addSubview(banner)
+            return banner
+        }
 
-    lazy var configureOnce: Void = {
+        return bannerViews
     }()
+    
+    var imageViews: [UIImageView] {
+        return bannerViews.map { $0.imageView }
+    }
+    
+    private var scrollViewObserving: NSKeyValueObservation!
+    
+    private var isObserving = true
+    private func withOutObserving(handler: () -> Void) {
+        objc_sync_enter(isObserving)
+        defer { objc_sync_exit(isObserving) }
+        
+        isObserving = false
+        handler()
+        isObserving = true
+    }
+    
+    lazy var configureOnce: Void = {
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.tapAction(_:))))
+        scrollView.addObserver(self, forKeyPath: #keyPath(UIScrollView.contentOffset), options: [.new, .old], context: nil)
+        originalOffset = isCircle ? 1 : 0
+    }()
+    
+    private func reloadBanner() {
+        if isCircle {
+            let prev = (currentIndex + numberOfImages - 1) % numberOfImages
+            let next = (currentIndex + 1) % numberOfImages
+            
+            zip(imageViews, [prev, currentIndex, next]).forEach { (imageView, index) in
+                guard let image = imageAtIndex(index) else { return }
+                if case let .memory(localImage) = image {
+                    imageView.image = localImage
+                }
+            }
+        }
+    }
+    
+    deinit {
+        scrollView.removeObserver(self, forKeyPath: #keyPath(UIScrollView.contentOffset))
+    }
+    
+    private var _currentIndex: Int = 0
+    var currentIndex: Int {
+        set {
+            _currentIndex = newValue
+            sendActions(for: .valueChanged)
+            reloadBanner()
+        }
+        get {
+            return numberOfImages > 0 ? (_currentIndex % numberOfImages + numberOfImages) % numberOfImages : 0
+        }
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard isObserving else { return }
+        guard !isBoundsChanging else { return }
+        guard numberOfImages > 0 else { return }
+
+        guard let contentOffset = change?[NSKeyValueChangeKey.newKey] as? CGPoint else { return }
+        guard let oldValue = change?[NSKeyValueChangeKey.oldKey] as? CGPoint else { return }
+        guard contentOffset != oldValue else { return }
+        var delta = Double((contentOffset.x - bounds.width * CGFloat(originalOffset)) / bounds.width)
+        delta = min(max(delta, -1), 1)
+
+        progress = delta
+    }
+
+    @objc private func tapAction(_ sender: Any) {
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -57,31 +225,97 @@ class CCBanner: UIControl, CCDrawable {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        isBoundsChanging = true
+        defer { isBoundsChanging = false }
+
         scrollView.frame = bounds
+        scrollView.contentSize = CGSize(width: bounds.width * 3, height: bounds.height)
+        
+        var divided = CGRect(origin: .zero, size: scrollView.contentSize).divided(atDistance: bounds.width, from: .minXEdge)
+        bannerViews.forEach { $0.frame = divided.slice; divided = divided.remainder.divided(atDistance: bounds.width, from: .minXEdge) }
+        
+        cover.frame = bounds
+
+        scrollView.contentOffset = CGPoint(x: bounds.width * CGFloat(originalOffset), y: 0)
     }
     
+    private var isBoundsChanging: Bool = false
+    override var bounds: CGRect {
+        willSet {
+            isBoundsChanging = true
+        }
+        didSet {
+            isBoundsChanging = false
+        }
+    }
+
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         let drawable: CCDrawable = delegate ?? self
-        drawable.drawableView(self, atRect: rect)
+        drawable.drawableView?(self, atRect: rect)
     }
     
     func drawableView(_ view: UIView, atRect rect: CGRect) {
         // Draw anything you want.
-        NSAttributedString(string: "mangonob").draw(at: .zero)
+        NSAttributedString(string: "Please draw page indicator for me.").draw(at: .zero)
     }
 }
 
 
-class CCBannerView: UIImageView {
+class CCBannerView: UIView {
+    var drawable: CCDrawable? {
+        didSet {
+            cover.drawable = drawable
+        }
+    }
+    
+    fileprivate lazy var cover: CCBannerCoverView = {
+        let cover = CCBannerCoverView()
+        cover.backgroundColor = UIColor.clear
+        cover.isUserInteractionEnabled = false
+        addSubview(cover)
+        return cover
+    }()
+    
+    override var contentMode: UIViewContentMode {
+        didSet {
+            imageView.contentMode = contentMode
+        }
+    }
+
+    var imageViewInsets: UIEdgeInsets = .zero {
+        didSet {
+            layoutSubviews()
+        }
+    }
+    
     fileprivate lazy var imageView: UIImageView = {
         let imageView = UIImageView()
-        addSubview(imageView)
+        insertSubview(imageView, at: 0)
         return imageView
     }()
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setNeedsDisplay()
+        imageView.frame = CGRect(x: imageViewInsets.left,
+                                 y: imageViewInsets.right,
+                                 width: bounds.width - (imageViewInsets.left + imageViewInsets.right),
+                                 height: bounds.height - (imageViewInsets.top + imageViewInsets.bottom))
+        cover.frame = bounds
     }
 }
 
+
+fileprivate class CCBannerCoverView: UIView {
+    var drawable: CCDrawable? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        drawable?.drawableView?(self, atRect: rect)
+    }
+}
--- Banner/ViewController.swift
+++ Banner/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var banner: CCBanner!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        banner.dataSource = self
+        banner.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
 
+extension ViewController: CCBannerDelegate {
+}
+
+extension ViewController: CCBannerDataSource {
+    func numberOfImages(inBanner banner: CCBanner) -> Int {
+        return 5
+    }
+    
+    func banner(_ banner: CCBanner, imageAtIndex index: Int) -> CCBannerImage? {
+        var images = [#imageLiteral(resourceName: "image1"), #imageLiteral(resourceName: "image2"), #imageLiteral(resourceName: "image3"), #imageLiteral(resourceName: "image4"), #imageLiteral(resourceName: "image5")].map({ CCBannerImage.memory($0)})
+        return images[index]
+    }
 }
 
